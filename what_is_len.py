ef51a929298d1bd2cdf25c0b432e8a30f0adc6ba
むずでょ <muzudho1@gmail.com>
2024-09-23 21:33:43 +0900

    range と len

---
+++ what_is_len.py
@@ -0,0 +1,39 @@
+#
+# python what_is_len.py
+#
+# len(x) って何だろう？
+#
+import traceback
+
+
+########################################
+# コマンドから実行時
+########################################
+if __name__ == '__main__':
+    try:
+        print(f"""\
+リストの長さを調べよう！
+
+{len([])}
+
+{len([1])}
+
+{len([1, 2])}
+
+{len([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])}
+
+{len(range(1, 11))}
+
+{len(list(range(1, 11)))}
+
+{len([i for i in range(1, 11)])}""")
+
+
+    except Exception as err:
+        print(f"""\
+おお、残念！　例外が投げられてしまった！  
+{err=}  {type(err)=}
+
+以下はスタックトレース表示じゃ。
+{traceback.format_exc()}
+""")
---
+++ what_is_range.py
@@ -0,0 +1,31 @@
+#
+# python what_is_range.py
+#
+# range(1, 10) って何だろう？
+#
+import traceback
+
+
+########################################
+# コマンドから実行時
+########################################
+if __name__ == '__main__':
+    try:
+        print(f"""\
+1 から 10 までの数が入ったリストを作ってみよう！
+
+{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+
+{list(range(1, 11))}
+
+{[i for i in range(1, 11)]}""")
+
+
+    except Exception as err:
+        print(f"""\
+おお、残念！　例外が投げられてしまった！  
+{err=}  {type(err)=}
+
+以下はスタックトレース表示じゃ。
+{traceback.format_exc()}
+""")
