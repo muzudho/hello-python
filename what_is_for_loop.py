d0e8a5c59bd3259925135bfe278f5347c7fc836a
むずでょ <muzudho1@gmail.com>
2024-10-04 16:21:55 +0900

    文章調整

--- what_is_for_loop.py
+++ what_is_for_loop.py
@@ -41,8 +41,8 @@ for i in range(1, 11):
 
     except Exception as err:
         print(f"""\
-おお、残念！　例外が投げられてしまった！  
-{err=}  {type(err)=}
+おお、残念！　例外が投げられてしまった！
+{type(err)=}  {err=}
 
 以下はスタックトレース表示じゃ。
 {traceback.format_exc()}
